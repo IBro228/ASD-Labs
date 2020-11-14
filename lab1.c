10b4cf9c3ec53f5220ce2c2827d3d429da612b37
IBro228 <ibrakhim.amelin@gmail.com>
2020-11-15 00:28:37 +0200

    Add two labs

---
+++ lab1.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+float first(float x) {
+    return 10 * x * x * x + 7 * x / 5 + 2;
+}
+
+float second(float x) {
+    return -x + 9;
+}
+
+int main() {
+    float x;
+    for (int i = 0; i < 11; i++) {
+        printf("Input any number:");
+        scanf_s("%f", &x);
+        printf("result:");
+        if ((x >= -49 && x < -10) || (x > 0 && x <= 10))
+            printf("%f", first(x));
+        else if (x > 20)
+            printf("%f", second(x));
+        else
+            printf("X does not belong to the given interval");
+        printf("\n");
+    }
+    return 0;
+}
